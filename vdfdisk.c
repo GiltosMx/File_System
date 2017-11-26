9aef1f8957c558920257a49e2ecaf611e2b0998e
GiltosMx <adriantoscanop@gmail.com>
2017-11-26 12:53:33 -0600

    Writes the MBR.

--- vdfdisk.c
+++ vdfdisk.c
@@ -20,21 +20,45 @@ struct MBR {
 
 int main() {
 	struct MBR mbr;
-	printf("%ld\n",sizeof(struct MBR));
 
-	// Crear la primera partición que será desde
-	// 	Cilindro= 0, Superficie = 0, Sector Físico = 2
+	for (size_t i = 0; i < 446; i++) {
+		mbr.bootstrap_code[i] = 0;
+	}
+
+	mbr.signature = 0x55AA;
+
+	mbr.partition[0].drive_status = 128 & 0xFF;
 	mbr.partition[0].chs_begin[0]=0; // Superficie
 	mbr.partition[0].chs_begin[1]=2 & 0x1F;	// Los 5 bits menos significativos son el sector
 	mbr.partition[0].chs_begin[2]=0; // Cilindro
 
+	mbr.partition[0].partition_type = 0x08;
 	mbr.partition[0].chs_end[0]=8; // Superficie
 	mbr.partition[0].chs_end[1]=27 & 0x1F;	// Los 5 bits menos significativos son el sector
 	mbr.partition[0].chs_end[2]=199 & 0xFF;	// Cilindro
 	mbr.partition[0].chs_end[1]=mbr.partition[0].chs_end[1] |
 								(199 & 0x300) >> 2;
 
+	mbr.partition[0].lba = 2 & 0xFFFFFFFF;
+	mbr.partition[0].secs_partition = 43199 & 0xFFFFFFFF;
+
 	printf("chs_end de la primera partición %X %X %X\n",mbr.partition[0].chs_end[0],mbr.partition[0].chs_end[1],mbr.partition[0].chs_end[2]);
+
+	for (size_t i = 1; i < 4; i++) {
+		mbr.partition[i].drive_status = 0;
+		mbr.partition[i].chs_begin[0]=0; // Superficie
+		mbr.partition[i].chs_begin[1]=0;	// Los 5 bits menos significativos son el sector
+		mbr.partition[i].chs_begin[2]=0; // Cilindro
+
+		mbr.partition[i].partition_type = 0;
+		mbr.partition[i].chs_end[0]=0; // Superficie
+		mbr.partition[i].chs_end[1]=0;	// Los 5 bits menos significativos son el sector
+		mbr.partition[i].chs_end[2]=0;	// Cilindro
+		mbr.partition[i].chs_end[1]=0;
+
+		mbr.partition[i].lba = 0 & 0xFFFFFFFF;
+		mbr.partition[i].secs_partition = 0 & 0xFFFFFFFF;
+	}
 
 	//Usar vdwritesector para escribir el MBR en el sector fisico 1 del disco
 	vdwritesector(0, 0, 0, 1, 1, (char*) &mbr);
