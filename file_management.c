5d925bf71c126734b04d0038224d4dc70f4efb96
GiltosMx <adriantoscanop@gmail.com>
2017-12-01 00:27:53 -0600

    Adds some file management functions.

---
+++ file_management.c
@@ -0,0 +1,254 @@
+#include "file_management.h"
+
+struct OPENFILES openfiles_table[16];
+struct INODE inode_table[MAX_INODES];
+struct SECBOOTPART sbp;
+unsigned char inodesmap[512];
+
+int openfiles_table_loaded = 0;
+int inode_table_loaded = 0;
+int sbp_loaded = 0;
+
+void checkOpenFilesTable() {
+    if (!openfiles_table_loaded) {
+        for (size_t i = 3; i < 16; i++) {
+            openfiles_table[i].inuse = 0;
+            openfiles_table[i].currbloqueenmemoria = -1;
+        }
+        openfiles_table_loaded = 1;
+    }
+}
+
+unsigned short *postoptr(int fd, int pos) {
+    int currinode;
+    unsigned short* currptr;
+    unsigned short indirect1;
+
+    currinode = openfiles_table[fd].inode;
+    if(!sbp_loaded) {
+        checkSecboot(&sbp);
+        sbp_loaded = 1;
+    }
+    if(!inode_table_loaded) {
+        checkiNodesTable(&sbp, inode_table);
+        inode_table_loaded = 1;
+    }
+
+    // Si esta en los primeros 10 k
+    if (pos / 1024 < 10) {
+        // Esta entre los 10 apuntadores directos
+        currptr = &inode_table[currinode].direct_blocks[pos / 1024];
+    } else if (pos / 1024 < 522) {
+        // Si el indirecto esta vacio, asignar un bloque
+        if (inode_table[currinode].indirect == 0) {
+            // El primer bloque disponible
+            indirect1 = nextfreeblock();
+            assignblock(indirect1);
+            inode_table[currinode].indirect = indirect1;
+        }
+        currptr = &openfiles_table[fd].buffindirect[pos / 1024 - 10];
+    } else
+        return NULL;
+
+    return currptr;
+}
+
+unsigned short* currpostoptr(int fd) {
+    unsigned short* currptr;
+    currptr = postoptr(fd, openfiles_table[fd].currpos);
+
+    return currptr;
+}
+
+int vdcreat(char* filename, unsigned short perms) {
+    int numinode = 0, i = 0;
+
+    // Ver si ya existe el archivo
+    numinode = searchinode(filename);
+
+    // Si no existe
+    if(numinode == -1) {
+        // Buscar un nodo i disponible en el mapa de bits
+        numinode = nextfreeinode();
+        // Si ya no hay nodos disponibles
+        if(numinode == -1) {
+            return (-1);
+        }
+    } else
+        removeinode(numinode);
+
+    // Escribir el archivo en el nodo encontrado
+    setinode(numinode, filename, perms, 1, 1);
+
+    // Poner el archivo en la tabla de archivos abiertos
+    // Establecer el archivo como abierto
+    checkOpenFilesTable();
+
+    // Buscar si hay lugar en la tabla de archivos abiertos
+    i = 3;
+    while (openfiles_table[i].inuse && i < 16)
+        i++;
+
+    if (i >= 16) // Llegamos al final y no hay lugar
+        return (-1);
+
+    openfiles_table[i].inuse = 1; // Poner el archivo en uso
+    openfiles_table[i].inode = numinode; // Nodo del archivo abierto
+    openfiles_table[i].currpos = 0; // Siempre es 0 al incio
+
+    return i;
+}
+
+int vdunlink(char * filename) {
+    int numinode = 0, i = 0;
+
+    numinode = searchinode(filename);
+    if (numinode == -1)
+        return (-1);
+
+    return removeinode(numinode);
+}
+
+int vdseek(int fd, int offset, int whence) {
+    unsigned short oldblock = 0, newblock = 0;
+
+    checkOpenFilesTable();
+    if (openfiles_table[fd].inuse == 0)
+        return (-1);
+
+    oldblock = *currpostoptr(fd);
+
+    // Si es a partir del inicio
+    if (whence == 0) {
+        // Si el offset esta antes del inicio o despues del final, error
+        if (offset < 0 || offset > inode_table[openfiles_table[fd].inode].size)
+            return (-1);
+
+        openfiles_table[fd].currpos = offset;
+    } else if (whence == 1) { // Es a partir de la posicion actual
+        // Validar que no se quiera mover antes del inicio del archivo
+        // o despues del final
+        if (openfiles_table[fd].currpos + offset >
+            inode_table[openfiles_table[fd].inode].size ||
+            openfiles_table[fd].currpos + offset < 0) {
+            return (-1);
+        }
+        openfiles_table[fd].currpos += offset;
+
+    } else if (whence == 2) { // A partir del final
+        if (offset > inode_table[openfiles_table[fd].inode].size ||
+            openfiles_table[fd].currpos - offset < 0) {
+            return (-1);
+        }
+        openfiles_table[fd].currpos = inode_table[openfiles_table[fd].inode].size - offset;
+    } else
+        return (-1);
+
+    // Verificar si la nueva posicion del apuntador es un bloque diferente al
+    // que estabamos. Si es asi, hay que cargar ese bloque a memoria
+    newblock = *currpostoptr(fd);
+
+    // Despues de mover el apuntador, me cambie a otro bloque?
+    if (newblock != oldblock) {
+        // Escribir el bloque viejo
+        writeblock(oldblock, openfiles_table[fd].buffer);
+        // Leer el bloque nuevo
+        readblock(newblock, openfiles_table[fd].buffer);
+        // Indicar el nuevo bloque como bloque actual
+        // en la tabla de archivos abiertos
+        openfiles_table[fd].currbloqueenmemoria = newblock;
+    }
+    // Regresar la posicion actual del archivo
+    return openfiles_table[fd].currpos;
+}
+
+int vdwrite(int fd, char *buffer, int bytes) {
+    int currblock, currinode, cont = 0, sector, result;
+    unsigned short* currptr;
+
+    checkSecboot(&sbp);
+    unsigned char inicio_nodos_i = checkiNodesMap(&sbp, inodesmap);
+    checkOpenFilesTable();
+
+    //Si no esta abierto regresa error
+    if (openfiles_table[fd].inuse == 0)
+        return (-1);
+
+    currinode = openfiles_table[fd].inode;
+
+    // Copiar byte por byte del buffer que recibe como
+    // argumento al buffer del archivo
+    while (cont < bytes) {
+        // Obtener la direccion de donde esta el bloque
+        // correspondiente a la posicion actual
+        currptr = currpostoptr(fd);
+        if (currptr == NULL)
+            return (-1);
+
+        // Cual es el bloque en el que escribiriamos
+        currblock = *currptr;
+
+        // Si el bloque esta en blanco, dale uno
+        if (currblock == 0) {
+            currblock = nextfreeblock();
+            // El bloque encontrado ponerlo en donde apunta
+            // el apuntador al bloque actual
+            *currptr = currblock;
+            // Asignarlo en el mapa de bits
+            assignblock(currblock);
+
+            // Escribir el sector de la tabla de nodos i en el disco
+            sector = currinode / 8;
+            result = vdwriteseclog(inicio_nodos_i + sector,
+                                   (char *) &inode_table[sector * 8]);
+        }
+        // Si el bloque de la posicion actual no esta en memoria
+        // Lee el bloque al buffer del archivo
+        if (openfiles_table[fd].currbloqueenmemoria != currblock) {
+            // Leer el bloque actual hacia el buffer que
+            // esta en la tabla de archivos abiertos
+            readblock(currblock, openfiles_table[fd].buffer);
+            // Actualizar en la taba de archivos abiertos
+            // el bloque actual
+            openfiles_table[fd].currbloqueenmemoria = currblock;
+        }
+
+        // Copia el caracter al buffer
+        openfiles_table[fd].buffer[openfiles_table[fd].currpos % (512*2)]
+         = buffer[cont];
+
+         // Incrementa posicion actual del cursor
+         openfiles_table[fd].currpos++;
+
+         // Si la posicion es mayor que el cambio, modifica el tamaño
+         if (openfiles_table[fd].currpos > inode_table[currinode].size) {
+            inode_table[openfiles_table[fd].inode].size
+            = openfiles_table[fd].currpos;
+        }
+
+        // Incrementa el contador
+        cont++;
+
+        // Si se llena el buffer, escribirlo
+        if (openfiles_table[fd].currpos % (512*2) == 0)
+            writeblock(currblock, openfiles_table[fd].buffer);
+    }
+    return cont;
+}
+
+int vdread(int fd, char *buffer, int bytes) {
+
+}
+
+int vdclose(int fd) {
+
+}
+
+int vdopen(char *filename,unsigned short mode) {
+
+}
+
+int main(int argc, char const *argv[]) {
+
+    return 0;
+}
---
+++ file_management.h
@@ -0,0 +1,42 @@
+#ifndef _FILEMGMT_H_
+#define _FILEMGMT_H_
+
+#include "inode_functions.h"
+
+struct OPENFILES {
+    int inuse;// 0 cerrado, 1 abierto
+    unsigned short inode;
+    int currpos;
+    int currbloqueenmemoria;
+    char buffer[1024];
+    unsigned short buffindirect[512];
+};
+
+void checkOpenFilesTable();
+
+// A partir de la posicion actual del apuntador del archivo, regresa la
+// direccion de memoria en el nodo i o en el bloque donde esta el
+// puntero del archivo
+unsigned short* currpostoptr(int fd);
+
+// Esta funcion se usa con las funciones seek, write y read. Con estas
+// operaciones el nodo i del archivo debe estar cargado en RAM. Por ejemplo,
+// si escribimos informacion en el archivo, se le empiezan a asignar bloques;
+// cuales? Los que encontremos disponibles, y esos bloques disponibles hay
+// que escribirlos en el nodo i que esta en memoria. Con esta funcion sabemos
+// cual es la direccion donde debe ir ese apuntador.
+unsigned short *postoptr(int fd,int pos);
+
+int vdcreat(char* filename,unsigned short perms);
+int vdopen(char* filename,unsigned short mode);
+int vdwrite(int fd, char *buffer, int bytes);
+int vdread(int fd, char *buffer, int bytes);
+int vdclose(int fd);
+int vdopen(char *filename,unsigned short mode);
+int vdunlink(char* filename);
+int vdseek(int fd, int offset, int whence);
+int vdwrite(int fd, char* buffer, int bytes);
+int vdread(int fd, char* buffer, int bytes);
+int vdclose(int fd);
+
+#endif
